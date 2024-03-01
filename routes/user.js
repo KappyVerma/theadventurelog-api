bdd6b55f048e479c6e75790d1d6b69f9650b1594
Karan Verma <karranverma@gmail.com>
2024-03-01 10:06:44 -0800

    change in response in user route

--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,7 @@ router
   .get(async (_req, res) => {
     try {
       const data = await knex("user");
-      res.status(200).json(data); //changes made here (req.body)
+      res.status(200).json(req.body); //changes made here (req.body) to data
     } catch (err) {
       res.status(400).send("Error while retrieving data from server");
     }
